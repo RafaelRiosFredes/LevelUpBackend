802b8c1eb1efb7e1f377dcbca1013dbf335a085c
RafaelRiosFredes <raf.rios@duocuc.cl>
2025-11-12 11:15:13 -0300

    Primer commit

---
+++ src/test/java/com/duoc/LevelUp/LevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.LevelUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
